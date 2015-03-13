d48f731011db0bbc4e6f82d2aa3dba743ee4fcce
Arnel <kod@MacBook-Pro.local>
2015-03-13 09:15:40 +0100

    first

---
+++ Casks/Sylpheed.rb
@@ -0,0 +1,10 @@
+cask :v1 => 'Sylpheed' do
+  version '3.4.1'
+
+  url "http://iij.dl.sourceforge.jp/sylpheed/60991/Sylpheed_#{version.gsub('.','')}-test1.dmg"
+  name 'Sylpheed'
+  homepage 'http://sylpheed.sourceforge.jp/'
+  license :GPL
+
+  app 'Sylpheed.app'
+end
