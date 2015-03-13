b8da94e9aaa41e1aeea6a7295d22dbd93b651447
Arnel <kod@MacBook-Pro.local>
2015-03-13 09:30:23 +0100

    sha256

--- Casks/Sylpheed.rb
+++ Casks/Sylpheed.rb
@@ -1,7 +1,9 @@
 cask :v1 => 'Sylpheed' do
   version '3.4.1'
+  sha256 '52505ab7913fd58fa921a9c8b574286f08073321b440b7aa23dba3896b4b2ddc'
+  
+  url 'http://iij.dl.sourceforge.jp/sylpheed/60991/Sylpheed_3.4.1-test1.dmg'
 
-  url "http://iij.dl.sourceforge.jp/sylpheed/60991/Sylpheed_#{version.gsub('.','')}-test1.dmg"
   name 'Sylpheed'
   homepage 'http://sylpheed.sourceforge.jp/'
   license :gpl
