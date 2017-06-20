bf91810c16b6da9d639119345e8f3a236cc20914
Tramezzino <tramezzino@tramezzino.local>
2017-06-20 15:13:24 +0200

    ColorSquid 1.2.2

---
+++ Casks/colorsquid.rb
@@ -0,0 +1,12 @@
+cask :v1 => 'ColorSquid' do
+  version '1.2.2'
+  sha256 '0da3a782fa3000e931c0b26ee25a80c64489371bd81cafddd7488cac40fdeebf'
+  
+  url 'http://apprepo.site.dev/ColorSquid.zip'
+
+  name 'ColorSquid'
+  homepage 'http://sylpheed.sourceforge.jp/'
+  license :gpl
+
+  app 'ColorSquid.app'
+end
