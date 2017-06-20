ffe1cfde1c55b0ae6ac1bd001bd8c6dab9172dee
Tramezzino <tramezzino@tramezzino.local>
2017-06-20 15:25:31 +0200

    fix

--- Casks/colorsquid.rb
+++ Casks/colorsquid.rb
@@ -1,12 +1,12 @@
-cask 'ColorSquid' do
+cask 'colorsquid' do
   version '1.2.2'
-  sha256 '0da3a782fa3000e931c0b26ee25a80c64489371bd81cafddd7488cac40fdeebf'
+  sha256 '180282b2ff7a31dc1dc19e41eeb04ed3917d7268aee15a18697de9997f67b004'
   
-  url 'http://apprepo.site.dev/ColorSquid.zip'
+  url 'http://apprepo.site.dev/colorsquid.zip'
 
-  name 'ColorSquid'
+  name 'colorsquid'
   homepage 'http://apprepo.site.dev/'
   license :gpl
 
-  app 'ColorSquid.app'
+  app 'colorsquid.app'
 end
