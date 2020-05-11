85228fbcfe05d12108f42515a249954f101f54c0
ahelja@alpenite.com <ahelja@alpenite.com>
2020-05-11 15:48:34 +0200

    Create macjournal.rb

---
+++ Casks/macjournal.rb
@@ -0,0 +1,34 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Macjournal < Formula
+  desc "Create, maintain, and blog a personal journal"
+  homepage "https://danschimpf.com"
+  url "https://danschimpf.com/macjournal/MacJournal_7.0.zip"
+  sha256 "5028e1a62681a7837332d064b46fe8e051fa530a04de80cf34df445d63b2d076"
+
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    # system "cmake", ".", *std_cmake_args
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test MacJournal`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
