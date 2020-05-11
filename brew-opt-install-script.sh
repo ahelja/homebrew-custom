320716eb92a6cb0ffcea7f3109f0fd2a89c23635
ahelja@alpenite.com <ahelja@alpenite.com>
2020-05-11 21:07:52 +0200

    Create brew-opt-install-script.sh

---
+++ brew-opt-install-script.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+
+# Homebrew Script for OSX
+# To execute: save and `chmod +x ./brew-install-script.sh` then `./brew-install-script.sh`
+
+# echo "Installing brew..."
+# /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew cask install ar-candelabrum
+brew cask install ar-pine
+brew cask install dd-utility
+brew cask install devcleaner
+brew cask install email-archiver-pro
+brew cask install fatpipe
+brew cask install fluid
+brew cask install fontplop
+brew cask install fontprep
+brew cask install icon-composer-2x
+brew cask install kaleidoscope
+brew cask install kindlegen_mac_i386_v2_9
+brew cask install licecap
+brew cask install macpass
+brew cask install mandrake
+brew cask install monolingual
+brew cask install newfilemenufree
+brew cask install onyx
+brew cask install openaudible
+brew cask install packages
+brew cask install parallax-previewer
+brew cask install purevpn
+brew cask install qcad-pro
+brew cask install repackage
+brew cask install resilio-sync
+brew cask install retinizer
+brew cask install robofont
+brew cask install screenflow-5.0.7
+brew cask install scummvm
+brew cask install sd-card-formatter
+brew cask install simplecleaner
+brew cask install skype
+brew cask install table-tool
+brew cask install teamviewer
+brew cask install vidconvert
+brew cask install vlc
+brew cask install vscodium
+brew cask install wwdc
+brew cask install telegram
+brew cask install camtwist
+brew cask install soundflower
+brew cask install paintcode
+brew cask install amadine
+brew cask install shapes
+brew cask install vectorize
