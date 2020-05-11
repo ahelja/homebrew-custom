49e952f0f9c0dd5bcabf1d095f0410270b921dbc
ahelja@alpenite.com <ahelja@alpenite.com>
2020-05-11 21:07:42 +0200

    Create brew-install-script.sh

---
+++ brew-install-script.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+# Homebrew Script for OSX
+# To execute: save and `chmod +x ./brew-install-script.sh` then `./brew-install-script.sh`
+
+echo "Installing brew..."
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+echo "Installing brew cask..."
+brew tap caskroom/cask
+brew tap ahelja/homebrew-custom
+
+# Programming Languages
+brew install node
+
+# Dev Tools
+brew install git
+brew cask install sublime-text
+brew cask install github-desktop
+brew cask install ar-imageoptim
+brew cask install ar-pixelmator
+brew cask install ar-fastcommander
+
+# Communication Apps
+brew cask install microsoft-teams
+
+# Web Tools
+brew cask install google-chrome
+
+# Writing Apps
+brew install pandoc
+brew cask install ar-macjournal
+
+# Entertainment
+brew cask install ar-netnewswire
+
+# utility
+brew cask install keepingyouawake
+brew cask install ar-appcleaner
+brew cask install cyberduck
+brew cask install ocenaudio
+brew cask install sketch
