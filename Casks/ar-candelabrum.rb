2cec2976f0aa955a1b20408b0d7bfca187259e44
ahelja@alpenite.com <ahelja@alpenite.com>
2020-05-11 20:43:03 +0200

    Update ar-candelabrum.rb

--- Casks/ar-candelabrum.rb
+++ Casks/ar-candelabrum.rb
@@ -1,6 +1,6 @@
 cask 'ar-candelabrum' do
   version '01e14de53287bae9e9fbb152fb15ee88'
-  sha256 '590db1aca8f0912548a34ba3f266afce34e92f4a0f6dc34f6fc598e55c5455cf'
+  sha256 '702805c20fcc2e949f211db4f0b01c3f41df31d9ea044265e213c3443b1a5c70'
 
   url "https://trello-attachments.s3.amazonaws.com/57593e1b83f68f626a3a69e3/5eb99b7728e76933a0e9950b/#{version}/Candelabrum.zip"
   name 'Candelabrum'
