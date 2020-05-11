c9a54d44a3fdde94adc66404cedcb2dd31bfe996
ahelja@alpenite.com <ahelja@alpenite.com>
2020-05-11 17:49:24 +0200

    Create dl.rb

---
+++ Casks/dl.rb
@@ -0,0 +1,34 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Dl < Formula
+  desc ""
+  homepage ""
+  url "https://trello-attachments.s3.amazonaws.com/57593e1b83f68f626a3a69e3/5e8342d5254d60771bd8e56b/f8b25ceac56c44cb337b0b27a27143d3/dl-1010.tar.gz"
+  sha256 "ada615b90627b99353627ae2997d670f83cdfe5feb07faa542f34da2c7266c21"
+
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    # system "cmake", ".", *std_cmake_args
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test dl`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
